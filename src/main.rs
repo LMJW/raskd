7e419a3e02e6fe5cfb7078f98f1de9272240f384
LMJW <heysuperming@gmail.com>
2020-04-19 12:20:27 +1000

    Update default db name

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ async fn main() -> std::io::Result<()> {
     // std::env::set_var("RUST_LOG", "actix_web=info");
     // env_logger::init();
     // let sql = fs::read_to_string("./src/sql/0001.sql")?;
-    let conn = r2d2_sqlite::SqliteConnectionManager::file("test.db");
+    let conn = r2d2_sqlite::SqliteConnectionManager::file("raskd.db");
     let pool = r2d2::Pool::new(conn).unwrap();
     HttpServer::new(move || {
         App::new()
