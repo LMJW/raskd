c348ecb98963b6b2a46a2cc3a5553d453dbfc793
LMJW <heysuperming@gmail.com>
2020-04-19 15:04:16 +1000

    Fix the dependence

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use r2d2;
 use r2d2_sqlite;
 mod db;
 mod handlers;
+use std::env;
 use std::fs;
 
 #[actix_rt::main]
@@ -16,10 +17,24 @@ async fn main() -> std::io::Result<()> {
         .subcommand(clap::App::new("start").about("start the raskd server"))
         .get_matches();
 
+    let mut path = env::current_exe()?;
+    path.pop();
+    path.push("raskd.db");
+    let path = path.to_str().unwrap();
     match cmds.subcommand() {
         ("init", Some(_)) => {
-            let sql = fs::read_to_string("./src/sql/0001.sql")?;
-            let conn = rusqlite::Connection::open("raskd.db").unwrap();
+            let sql = r#"
+            CREATE TABLE tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT NOT NULL,
+                tasktype TEXT NOT NULL,
+                start TEXT NOT NULL,
+                stop TEXT
+            );
+            "#;
+            eprintln!("Your sqlite db is located at:");
+            eprintln!("`{}`", path);
+            let conn = rusqlite::Connection::open(path).unwrap();
             match conn.execute_batch(&sql) {
                 Ok(_) => {
                     eprintln!("Successfully generated sqlite3 database `raskd.db`.");
@@ -37,11 +52,11 @@ async fn main() -> std::io::Result<()> {
             // std::env::set_var("RUST_LOG", "actix_web=info");
             // env_logger::init();
             // let sql = fs::read_to_string("./src/sql/0001.sql")?;
-            if let Err(e) = fs::read("raskd.db") {
+            if let Err(e) = fs::read(path) {
                 eprintln!("database does not exist. use `raskd init` to create database");
                 return Err(e);
             };
-            let conn = r2d2_sqlite::SqliteConnectionManager::file("raskd.db");
+            let conn = r2d2_sqlite::SqliteConnectionManager::file(path);
             let pool = r2d2::Pool::new(conn).unwrap();
             HttpServer::new(move || {
                 App::new()
