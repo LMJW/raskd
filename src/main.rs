6f1c16184404856be113b3e94c74d3b93c60b7b4
LMJW <heysuperming@gmail.com>
2021-07-24 17:15:05 +1000

    Minor update

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,39 @@ mod db;
 mod handlers;
 use std::env;
 use std::fs;
+use std::path::PathBuf;
+use tracing::debug;
+use tracing_subscriber;
 
-#[actix_rt::main]
+fn create_database(path: &PathBuf) -> Result<(), std::io::Error> {
+    const SQL: &str = r#"
+    CREATE TABLE tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        tasktype TEXT NOT NULL,
+        start TEXT NOT NULL,
+        stop TEXT
+    );
+    "#;
+    eprintln!("Your sqlite db is located at:");
+    eprintln!("`{:?}`", path.as_os_str());
+    let conn = rusqlite::Connection::open(path).expect("unable to open sqlite connection");
+    match conn.execute_batch(&SQL) {
+        Ok(_) => {
+            eprintln!("Successfully generated sqlite3 database `raskd.db`.");
+            Ok(())
+        }
+        Err(e) => {
+            debug!("{}", e);
+            let err = std::io::Error::new(std::io::ErrorKind::Other, format!("{}", e));
+            Err(err)
+        }
+    }
+}
+
+#[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    tracing_subscriber::fmt::init();
     let cmds = clap::App::new("Raskd server")
         .version("0.1.0")
         .author("LMJW")
@@ -20,43 +50,15 @@ async fn main() -> std::io::Result<()> {
     let mut path = env::current_exe()?;
     path.pop();
     path.push("raskd.db");
-    let path = path.to_str().unwrap();
     match cmds.subcommand() {
-        ("init", Some(_)) => {
-            let sql = r#"
-            CREATE TABLE tasks (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT NOT NULL,
-                tasktype TEXT NOT NULL,
-                start TEXT NOT NULL,
-                stop TEXT
-            );
-            "#;
-            eprintln!("Your sqlite db is located at:");
-            eprintln!("`{}`", path);
-            let conn = rusqlite::Connection::open(path).unwrap();
-            match conn.execute_batch(&sql) {
-                Ok(_) => {
-                    eprintln!("Successfully generated sqlite3 database `raskd.db`.");
-                    eprintln!("You can now run `raskd start` to start the server.");
-                    Ok(())
-                }
-                Err(e) => {
-                    eprintln!("{}", e);
-                    let err = std::io::Error::new(std::io::ErrorKind::Other, format!("{}", e));
-                    Err(err)
-                }
-            }
-        }
+        ("init", Some(_)) => create_database(&path),
         _ => {
-            // std::env::set_var("RUST_LOG", "actix_web=info");
-            // env_logger::init();
-            // let sql = fs::read_to_string("./src/sql/0001.sql")?;
-            if let Err(e) = fs::read(path) {
-                eprintln!("database does not exist. use `raskd init` to create database");
-                return Err(e);
+            if let Err(e) = fs::read(&path) {
+                debug!("{}", e);
+                eprintln!("database does not exist. creating the database...");
+                create_database(&path)?;
             };
-            let conn = r2d2_sqlite::SqliteConnectionManager::file(path);
+            let conn = r2d2_sqlite::SqliteConnectionManager::file(&path);
             let pool = r2d2::Pool::new(conn).unwrap();
             HttpServer::new(move || {
                 App::new()
